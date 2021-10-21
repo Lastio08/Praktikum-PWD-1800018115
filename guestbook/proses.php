2e71483b6e968b01514a89cd14a8150f69a4d9e7
Lastio Aji Sefi Rahman - 1800018115 <48423218+Lastio08@users.noreply.github.com>
2021-10-21 21:15:37 +0700

    praktikum3

--- guestbook/proses.php
+++ guestbook/proses.php
@@ -1,14 +1,7 @@
 <?php
-
-$nama = $_POST['nama'];
-$alamat = $_POST['alamat'];
-$email = $_POST['email'];
-$status = $_POST['status'];
-$komentar = $_POST['komentar'];
-
 echo "<head><title>My Guest Book</head></title>";
 $fp = fopen("guestbook.txt","a+");
-fputs($fp,"$nama|$alamat|$email|$status|$komentar\n");
+//fputs($fp,"$nama|$alamat|$email|$status|$komentar\n");
 fclose($fp);
 
 echo "Terima kasih atas partisipasi anda mengisi buku tamu <br>";
